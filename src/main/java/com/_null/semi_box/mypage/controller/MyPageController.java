26740155dd1af37a13ebdfc221093437a596396d
fstwon <fstwon@gmail.com>
2025-04-10 15:35:03 +0900

    [feat]메인 헤더 마이페이지 버튼 비로그인/로그인 기능 구분

--- src/main/java/com/_null/semi_box/mypage/controller/MyPageController.java
+++ src/main/java/com/_null/semi_box/mypage/controller/MyPageController.java
@@ -7,6 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com._null.semi_box.member.model.vo.Member;
 
 @WebServlet("/mypage/inventory")
 public class MyPageController extends HttpServlet {
@@ -18,7 +21,14 @@ public class MyPageController extends HttpServlet {
 	 * @throws IOException
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Content directory: /src/main/webapp
-		request.getRequestDispatcher("/views/myPage/inventory.jsp").forward(request, response);
+		// 현재 로그인 정보 확인
+		HttpSession session = request.getSession();
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			request.getRequestDispatcher("/views/signPage/signIn.jsp").forward(request, response);	
+		} else {
+			request.getRequestDispatcher("/views/myPage/inventory.jsp").forward(request, response);
+		}
 	}
 }
